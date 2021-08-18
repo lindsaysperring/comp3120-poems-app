4950e34dfa91abd6ff30df19308cbd458c4e374c
Lindsay <sperringlindsay@gmail.com>
2021-08-18 21:52:06 +1000

    basic grid for poem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container app">
+      <h1>Awesome Poems</h1>
+      <div className="row poemGridContainer">
+        <div class="three columns">Col1</div>
+        <div class="three columns">Col2</div>
+        <div class="three columns">Col3</div>
+        <div class="three columns">Col4</div>
+      </div>
     </div>
   );
 }
